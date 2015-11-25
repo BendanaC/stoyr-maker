ac2e542d7bbc7d9df6a85d2286c72a993f71fda9
unknown <cmackm@hotmail.com>
2015-11-24 18:58:54 -0600

    Added two stories

--- www/js/story.js
+++ www/js/story.js
@@ -1,27 +1,58 @@
 var story = [
   {
     pageNumber: 0,
-    text: 'Lets start our first story!!',
+    text: 'Escoja el comienzo de su historia',
     options:[
-      {nextPage: 1, text: 'Start!'},
-      {nextPage: 0, text: 'Do not start :('},
-      {nextPage: 2, text: 'Go to the end'},
-      {nextPage: 1, text: 'Yes lets start'}
+      {nextPage: 1a, text: 'Había una vez un perrito Shiba Inu...'},
+      {nextPage: 1b, text: 'Érase una vez un estudiante de universidad...'},
+      {nextPage: 3a, text: 'Ir al primer final'},
+      {nextPage: 3b, text: 'Ir al segundo final'}
     ]
   },
   {
-    pageNumber: 1,
-    text: 'Great, lets do this',
+    pageNumber: 1a,
+    text: 'De color amarillo y joven que le gustaba jugar con pelotas de tennis y con su amo Richard Gere.',
     options:[
-      {nextPage: 0, text: 'Leave'},
-      {nextPage: 2, text: 'Move forward'}
+      {nextPage: 2a, text: 'Un día decidió no hacer nada más que dormir...'},
+      {nextPage: 2b, text: 'Su dueño cayó en una enfermedad muy grave y tuvo que ir al hospital...'}
     ]
   },
   {
-    pageNumber: 2,
-    text: 'This is the end, thank you!',
+    pageNumber: 1b,
+    text: 'Que estaba a final de cuatrimestre y no daba más. Algo tenía que hacer, ¿o no?',
     options:[
-      {nextPage: 0, text: 'Bye'}
+      {nextPage: 2a, text: 'Dejar la universidad y tomar un cuatrimestre sabático...'},
+      {nextPage: 2b, text: 'Ponerle aún más empeño a la universidad'}
+    ]
+  },
+ {
+    pageNumber: 2a,
+    text: 'En su plan de descanso, se encontró un fajo de billetes de $100.000 al lado de su casa',
+    options:[
+      {nextPage: 3a, text: 'Se lo entregó a la policía...'},
+      {nextPage: 3b, text: 'Gastó cada centavo...'}
+    ]
+  },
+{
+    pageNumber: 2b,
+    text: 'Cayó en una depresión tan terrible que no podía levantarse de su cama',
+    options:[
+      {nextPage: 3b, text: 'Por lo cual decidió no hacer nada nunca más...'},
+      {nextPage: 3a, text: 'Hizo el esfuerzo de levantarse y hacer sus quehaceres'}
+    ]
+  },
+{
+    pageNumber: 3a,
+    text: 'Y al salir de su casa, le cayó un rayo :(',
+    options:[
+      {nextPage: 0, text: 'Volver al principio'},
+    ]
+  },
+{
+    pageNumber: 3b,
+    text: 'Y quién lo diría, se convirtió en el ser más feliz del mundo! :)',
+    options:[
+      {nextPage: 0, text: 'Volver al principio'},
     ]
   },
 ];
