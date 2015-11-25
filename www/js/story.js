52f262c87ad3594790aedfca73aaced5e9e4da77
Carlos Jimenez <carlosjiben@gmail.com>
2015-11-24 23:56:50 -0600

    Added new page to show the selected story:
    - Moved index.html and index.js code to story.html and viewStory.js
    - The selected story is received via param.

--- www/js/story.js
+++ www/js/story.js
@@ -1,58 +1,109 @@
 var story = [
-  {
-    pageNumber: 0,
-    text: 'Escoja el comienzo de su historia',
-    options:[
-      {nextPage: 1a, text: 'HabÌa una vez un perrito Shiba Inu...'},
-      {nextPage: 1b, text: '…rase una vez un estudiante de universidad...'},
-      {nextPage: 3a, text: 'Ir al primer final'},
-      {nextPage: 3b, text: 'Ir al segundo final'}
-    ]
-  },
-  {
-    pageNumber: 1a,
-    text: 'De color amarillo y joven que le gustaba jugar con pelotas de tennis y con su amo Richard Gere.',
-    options:[
-      {nextPage: 2a, text: 'Un dÌa decidiÛ no hacer nada m·s que dormir...'},
-      {nextPage: 2b, text: 'Su dueÒo cayÛ en una enfermedad muy grave y tuvo que ir al hospital...'}
-    ]
-  },
-  {
-    pageNumber: 1b,
-    text: 'Que estaba a final de cuatrimestre y no daba m·s. Algo tenÌa que hacer, øo no?',
-    options:[
-      {nextPage: 2a, text: 'Dejar la universidad y tomar un cuatrimestre sab·tico...'},
-      {nextPage: 2b, text: 'Ponerle a˙n m·s empeÒo a la universidad'}
-    ]
-  },
- {
-    pageNumber: 2a,
-    text: 'En su plan de descanso, se encontrÛ un fajo de billetes de $100.000 al lado de su casa',
-    options:[
-      {nextPage: 3a, text: 'Se lo entregÛ a la policÌa...'},
-      {nextPage: 3b, text: 'GastÛ cada centavo...'}
-    ]
-  },
-{
-    pageNumber: 2b,
-    text: 'CayÛ en una depresiÛn tan terrible que no podÌa levantarse de su cama',
-    options:[
-      {nextPage: 3b, text: 'Por lo cual decidiÛ no hacer nada nunca m·s...'},
-      {nextPage: 3a, text: 'Hizo el esfuerzo de levantarse y hacer sus quehaceres'}
-    ]
-  },
-{
-    pageNumber: 3a,
-    text: 'Y al salir de su casa, le cayÛ un rayo :(',
-    options:[
-      {nextPage: 0, text: 'Volver al principio'},
-    ]
-  },
-{
-    pageNumber: 3b,
-    text: 'Y quiÈn lo dirÌa, se convirtiÛ en el ser m·s feliz del mundo! :)',
-    options:[
-      {nextPage: 0, text: 'Volver al principio'},
-    ]
-  },
+   {
+      id: 0,
+      name: 'Such story, very interesting',
+      pages: [
+         {
+            pageNumber: 0,
+            text: 'Escoja el comienzo de su historia',
+            options:[
+               {nextPage: 1, text: 'Hab√≠a una vez un perrito Shiba Inu...'},
+               {nextPage: 5, text: 'Ir al segundo final'},
+               {nextPage: 4, text: 'Ir al primer final'}
+            ]
+         },
+         {
+            pageNumber: 1,
+            text: 'De color amarillo y joven que le gustaba jugar con pelotas de tennis y con su amo Richard Gere.',
+            options:[
+               {nextPage: 2, text: 'Un d√≠a decidi√≥ no hacer nada m√°s que dormir...'},
+               {nextPage: 3, text: 'Su due√±o cay√≥ en una enfermedad muy grave y tuvo que ir al hospital...'}
+            ]
+         },
+         {
+            pageNumber: 2,
+            text: 'En su plan de descanso, se encontr√≥ un fajo de billetes de $100.000 al lado de su casa',
+            options:[
+               {nextPage: 4, text: 'Se lo entreg√≥ a la polic√≠a...'},
+               {nextPage: 5, text: 'Gast√≥ cada centavo...'}
+            ]
+         },
+         {
+            pageNumber: 3,
+            text: 'Cay√≥ en una depresi√≥n tan terrible que no pod√≠a levantarse de su cama',
+            options:[
+               {nextPage: 5, text: 'Por lo cual decidi√≥ no hacer nada nunca m√°s...'},
+               {nextPage: 4, text: 'Hizo el esfuerzo de levantarse y hacer sus quehaceres'}
+            ]
+         },
+         {
+            pageNumber: 4,
+            text: 'Y al salir de su casa, le cay√≥ un rayo :(',
+            options:[
+               {nextPage: '0', text: 'Volver al principio'},
+            ]
+         },
+         {
+            pageNumber: 5,
+            text: 'Y qui√©n lo dir√≠a, se convirti√≥ en el ser m√°s feliz del mundo! :)',
+            options:[
+               {nextPage: '0', text: 'Volver al principio'},
+            ]
+         }
+      ]
+   },
+
+   {
+      id: 1,
+      name: 'Estudiante indeciso',
+      pages: [
+         {
+            pageNumber: 0,
+            text: 'Escoja el comienzo de su historia',
+            options:[
+               {nextPage: 1, text: '√ârase una vez un estudiante de universidad...'},
+               {nextPage: 4, text: 'Ir al primer final'},
+               {nextPage: 5, text: 'Ir al segundo final'}
+            ]
+         },
+         {
+            pageNumber: 1,
+            text: 'Que estaba a final de cuatrimestre y no daba m√°s. Algo ten√≠a que hacer, ¬øo no?',
+            options:[
+               {nextPage: 2, text: 'Dejar la universidad y tomar un cuatrimestre sab√°tico...'},
+               {nextPage: 3, text: 'Ponerle a√∫n m√°s empe√±o a la universidad'}
+            ]
+         },
+         {
+            pageNumber: 2,
+            text: 'En su plan de descanso, se encontr√≥ un fajo de billetes de $100.000 al lado de su casa',
+            options:[
+               {nextPage: 4, text: 'Se lo entreg√≥ a la polic√≠a...'},
+               {nextPage: 5, text: 'Gast√≥ cada centavo...'}
+            ]
+         },
+         {
+            pageNumber: 3,
+            text: 'Cay√≥ en una depresi√≥n tan terrible que no pod√≠a levantarse de su cama',
+            options:[
+               {nextPage: 5, text: 'Por lo cual decidi√≥ no hacer nada nunca m√°s...'},
+               {nextPage: 4, text: 'Hizo el esfuerzo de levantarse y hacer sus quehaceres'}
+            ]
+         },
+         {
+            pageNumber: 4,
+            text: 'Y al salir de su casa, le cay√≥ un rayo :(',
+            options:[
+               {nextPage: '0', text: 'Volver al principio'},
+            ]
+         },
+         {
+            pageNumber: 5,
+            text: 'Y qui√©n lo dir√≠a, se convirti√≥ en el ser m√°s feliz del mundo! :)',
+            options:[
+               {nextPage: '0', text: 'Volver al principio'},
+            ]
+         }
+      ]
+   }
 ];
